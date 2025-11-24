4edf7e4d2c204a034c8f40b4c325deb728a5c5bd
LARGERONThibault <t.largeron@bdx.ecolescreatives.com>
2025-11-24 15:30:05 +0100

    REWORK enemy spawn + wave management
    Created 3 coroutines.
    - 2 to manage base and shield enemies spawn featuring the ammount of enemy to spawn and a new 'cooldown between each enemy spawned' public variable.
    - 1 called in start to manage waves with a new currentwave variable.
    
    Also created a system that cheks the enemy list everyframe to know when to skip to the next wave.

--- Project TOWER DEFENSE/Assets/MyStuff/Spawner.cs
+++ Project TOWER DEFENSE/Assets/MyStuff/Spawner.cs
@@ -11,32 +11,85 @@ public class Spawner : MonoBehaviour
     public Vector3 spawnoffset;
     Quaternion spawnrotation = new Quaternion (0, 180, 0, 0);
 
+    public float spawncooldown = 0.5f;
     List<GameObject> wave = new List<GameObject>();
+    int currentWave = 1;
+    int deadEnemies = 0;
 
-    void SpawnBase()
+    //Pour chaque ennemi qu'on veut faire spawn, le fait apparaître, l'ajoute à la liste et attend 0.5 secondes avant le suivant.
+    IEnumerator SpawnBase(int ammount)
     {
-       GameObject newmob = Instantiate(enemy1, (cameraposition.position + spawnoffset), spawnrotation);
-        wave.Add(newmob);
+        int numberSpawned = 0;
+        while (numberSpawned < ammount) 
+        { 
+            GameObject newmob = Instantiate(enemy1, (cameraposition.position + spawnoffset), spawnrotation);
+            wave.Add(newmob);
+            yield return new WaitForSeconds(spawncooldown);
+            numberSpawned++;
+        }
     }
 
-    void SpawnShield()
+    //Pareil avec l'autre type d'ennemi
+    IEnumerator SpawnShield(int ammount)
     {
-        Instantiate(enemy2, (cameraposition.position + spawnoffset), spawnrotation);
+        int numberSpawned = 0;
+        while (numberSpawned < ammount)
+        {
+            GameObject newmob = Instantiate(enemy2, (cameraposition.position + spawnoffset), spawnrotation);
+            wave.Add(newmob);
+            yield return new WaitForSeconds(spawncooldown);
+            numberSpawned++;
+        }
     }
 
-    
-
+    //Active la gestion des vagues dans le start.
+    private void Start()
+    {
+        StartCoroutine(Wave());
+    }
 
     private void Update()
     {
+        //Admin command pour faire spawn un enemmi.
         if (Input.GetKeyDown(KeyCode.L))
         {
-            SpawnBase();
+            StartCoroutine(SpawnBase(1));
         }
 
         if (Input.GetKeyDown(KeyCode.P))
         {
-            SpawnShield();
+            StartCoroutine(SpawnShield(1));
         }
+
+        //Check si la vague actuelle a été remportée par le joueur et si oui passe à la suivante.
+        deadEnemies = 0;
+        foreach (GameObject obj in wave)
+        {
+            if (obj == null) 
+            { 
+                deadEnemies++;
+            }
+        }
+        if (deadEnemies == wave.Count)
+        {
+            wave.Clear();
+            currentWave++;
+        }
+    }
+
+    //Gère les vagues d'ennemis
+    IEnumerator Wave()
+    {
+        StartCoroutine(SpawnBase(2));
+        while (currentWave != 2)
+        {
+            yield return null;
+        }
+        StartCoroutine(SpawnShield(1));
+        while (currentWave != 3)
+        {
+            yield return null;
+        }
+        Debug.Log("Skibidi toilets");
     }
 }
